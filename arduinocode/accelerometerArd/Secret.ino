f973ed022d3282a78ea80bb18fce961e228bb2b8
b-gandhi1 <bgandhi1@sheffield.ac.uk>
2024-04-11 18:13:19 +0100

    updates on accelerometer code and ros

---
+++ arduinocode/accelerometerArd/Secret.ino
@@ -0,0 +1,3 @@
+//please enter your sensitive data in the Secret tab
+char ssid[] = SECRET_SSID;                // your network SSID (name)
+char pass[] = SECRET_PASS;                // your network password (use for WPA, or use as key for WEP)
--- arduinocode/accelerometerArd/accelerometerArd.ino
+++ arduinocode/accelerometerArd/accelerometerArd.ino
@@ -1,43 +1,71 @@
-#include <Arduino_LSM6DS3.h> % library for Arduino Nano 33 IOT
+#include <Arduino_LSM6DS3.h> // library for Arduino Nano 33 IOT
+#include <WiFiNINA.h>
 
 // ROS ingration
-#include <ros.h>
-#include <std_msgs/Float32.h>
+//#include <ros.h>
+//#include <std_msgs/Int16.h>
+
+// wifi init: 
+int status = WL_IDLE_STATUS;             // the Wi-Fi radio's status
+int ledState = LOW;                       //ledState used to set the LED
+unsigned long previousMillisInfo = 0;     //will store last time Wi-Fi information was updated
+unsigned long previousMillisLED = 0;      // will store the last time LED was updated
+const int intervalInfo = 5000;            // interval at which to update the board information
 
 // ROS node handle
-ros::NodeHandle nh;
+//ros::NodeHandle nh;
 
 // ROS publisher define
-// publishing pump state (output) and pressure value (input) 
-std_msgs::Float32 msg1;
-std_msgs::Float32 msg2;
-ros::Publisher rotX("rotX",&msg1);
-ros::Publisher rotY("rotY",&msg2);
+//std_msgs::Int16 msg1;
+//std_msgs::Int16 msg2;
+//ros::Publisher rotX("rotX",&msg1);
+//ros::Publisher rotY("rotY",&msg2);
 
 float x, y, z; // z unused
-float degX = 0.0;
-float degY = 0.0;
+float degX = 0;
+float degY = 0;
 
 void setup() {
   int baudrate = 57600;
+//  int baudrate = 9600;
+//  int baudrate = 34800;
+//  int baudrate = 115200;
+  
   Serial.begin(baudrate);
-  while(!Serial);
-  Serial.println("Started");
+//  while(!Serial);
+//  Serial.println("Started");
+//
+//  if (!IMU.begin()){
+//    Serial.println("Failed to initialise IMU!");
+//    while(1);
+//  }
+//  
+//  Serial.print("Accelerometer sample rate = ");
+//  Serial.print(IMU.accelerationSampleRate());
+//  Serial.println("Hz");
 
-  if (!IMU.begin()){
-    Serial.println("Failed to initialise IMU!");
-    while(1);
+  // set the LED as output
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // attempt to connect to Wi-Fi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to network: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+  
+    // wait 10 seconds for connection:
+    delay(10000);
   }
-  
-  Serial.print("Accelerometer sample rate = ");
-  Serial.print(IMU.accelerationSampleRate());
-  Serial.println("Hz");
-  
+  // you're connected now, so print out the data:
+  Serial.println("You're connected to the network");
+  Serial.println("---------------------------------------");
+
   // ROS setup
-  nh.initNode();
-  nh.getHardware()->setBaud(baudrate);
-  nh.advertise(rotX);
-  nh.advertise(rotY);
+//  nh.initNode();
+//  nh.getHardware()->setBaud(baudrate);
+//  nh.advertise(rotX);
+//  nh.advertise(rotY);
   
   delay(100);
   
@@ -45,31 +73,81 @@ void setup() {
 
 void loop() {
 
+  
+  unsigned long currentMillisInfo = millis();
+  // check if the time after the last update is bigger the interval
+  if (currentMillisInfo - previousMillisInfo >= intervalInfo) {
+    previousMillisInfo = currentMillisInfo;
+
+    Serial.println("Board Information:");
+    // print your board's IP address:
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
+
+    // print your network's SSID:
+    Serial.println();
+    Serial.println("Network Information:");
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
+
+    // print the received signal strength:
+    long rssi = WiFi.RSSI();
+    Serial.print("signal strength (RSSI):");
+    Serial.println(rssi);
+    Serial.println("---------------------------------------");
+  }
+
+  unsigned long currentMillisLED = millis();
+
+  // measure the signal strength and convert it into a time interval
+  int intervalLED = WiFi.RSSI() * -10;
+ 
+  // check if the time after the last blink is bigger the interval 
+  if (currentMillisLED - previousMillisLED >= intervalLED) {
+    previousMillisLED = currentMillisLED;
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(LED_BUILTIN, ledState);
+  }
+  
   if (IMU.accelerationAvailable()) {
     IMU.readAcceleration(x, y, z);
     x = 100 * x;
     degX = map(x,-100,97,-90,90);
     y = 100 * y;
     degY = map(y,-100,97,-90,90);
+
+//    Serial.print("degX: ");
+//    Serial.print(degX);
+//    Serial.print("  degY: ");
+//    Serial.println(degY);
     // Publish to ROS
-    msg1.data = degX;
-    rotX.publish(&msg1);
-    msg2.data = degY;
-    rotY.publish(&msg2);
-  
-    nh.spinOnce();
-    delay(10);
+//    msg1.data = degX;
+//    rotX.publish(&msg1);
+//    msg2.data = degY;
+//    rotY.publish(&msg2);
+//  
+//    nh.spinOnce();
+//    delay(10);
   }
   else {
     Serial.println("ERROR: IMU disconnected!");
     // Publish to ROS
-    msg1.data = 404.404;
-    rotX.publish(&msg1);
-    msg2.data = 404.404;
-    rotY.publish(&msg2);
-  
-    nh.spinOnce();
-    delay(10);
+//    msg1.data = 404;
+//    rotX.publish(&msg1);
+//    msg2.data = 404;
+//    rotY.publish(&msg2);
+//  
+//    nh.spinOnce();
+//    delay(10);
   }
   
 }
